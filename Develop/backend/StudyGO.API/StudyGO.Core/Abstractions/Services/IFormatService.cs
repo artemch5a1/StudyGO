f8d4cfb78ca777ff44d1ddd06580295847f035af
Artem <alefa2000@mail.ru>
2025-07-31 21:33:28 +0300

    create two service

---
+++ Develop/backend/StudyGO.API/StudyGO.Core/Abstractions/Services/IFormatService.cs
@@ -0,0 +1,11 @@
+﻿using StudyGO.Contracts.Dtos.Formats;
+
+namespace StudyGO.Core.Abstractions.Services
+{
+    public interface IFormatService
+    {
+        public Task<List<FormatDto>> GetAllFormats();
+
+        public Task<FormatDto> GetFormatById();
+    }
+}
---
+++ Develop/backend/StudyGO.API/StudyGO.Core/Abstractions/Services/ISubjectService.cs
@@ -0,0 +1,11 @@
+﻿using StudyGO.Contracts.Dtos.Subjects;
+
+namespace StudyGO.Core.Abstractions.Services
+{
+    public interface ISubjectService
+    {
+        public Task<List<SubjectDto>> GetAllSubjects();
+
+        public Task<SubjectDto> GetSubjectById();
+    }
+}
