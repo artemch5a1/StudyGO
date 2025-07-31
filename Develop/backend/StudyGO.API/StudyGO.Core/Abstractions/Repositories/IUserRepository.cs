873d1651cb9a4ef497c8c710ea6cfa114090e661
Artem <alefa2000@mail.ru>
2025-07-31 21:59:50 +0300

    add first method

--- Develop/backend/StudyGO.API/StudyGO.Core/Abstractions/Repositories/IUserRepository.cs
+++ Develop/backend/StudyGO.API/StudyGO.Core/Abstractions/Repositories/IUserRepository.cs
@@ -8,7 +8,7 @@ namespace StudyGO.Core.Abstractions.Repositories
     {
         public Task<bool> Update(User user);
 
-        public Task<bool> Delete(int id);
+        public Task<bool> Delete(Guid id);
 
         public Task<UserLoginResponse> LoginAction(UserLoginRequest login);
     }
---
+++ Develop/backend/StudyGO.API/StudyGO.infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyGO.Contracts.Contracts;
+using StudyGO.Core.Abstractions.Repositories;
+using StudyGO.Core.Models;
+using StudyGO.infrastructure.Data;
+
+namespace StudyGO.infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            try
+            {
+                int count = await _context
+                    .UsersEntity.Where(x => x.UserID == id)
+                    .ExecuteDeleteAsync();
+                if (count != 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<User>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserLoginResponse> LoginAction(UserLoginRequest login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
