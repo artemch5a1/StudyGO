aec65b5ecf2da09460c1fbd13e195dc9c8461814
praktika <praktika@PRAKTIKA>
2025-08-01 11:28:46 +0300

    done repositories and registr and DI

---
+++ Develop/backend/StudyGO.API/StudyGO.API/Services/Repositories/ServiceBuilder.cs
@@ -0,0 +1,17 @@
+﻿using StudyGO.Core.Abstractions.Repositories;
+using StudyGO.infrastructure.Repositories;
+
+namespace StudyGO.API.Services
+{
+    public partial class ServiceBuilder
+    {
+        private static void ConfigureRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserProfileRepository, UserProfileRepository>();
+            services.AddScoped<ITutorProfileRepository, TutorProfileRepository>();
+            services.AddScoped<IFormatRepository, FormatRepository>();
+            services.AddScoped<ISubjectRepository, SubjectRepository>();
+        }
+    }
+}
