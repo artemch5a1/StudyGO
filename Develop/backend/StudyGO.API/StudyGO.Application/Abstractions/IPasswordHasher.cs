8784722b71d1a31952824dfe4d9f8c2952e3cdd7
praktika <praktika@PRAKTIKA>
2025-08-01 14:10:06 +0300

    create password hasher

---
+++ Develop/backend/StudyGO.API/StudyGO.Application/Abstractions/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace StudyGO.Application.Abstractions
+{
+    public interface IPasswordHasher
+    {
+        public string GeneratePasswordHash(string password);
+
+        public bool VerifiyPassword(string password, string passwordHash);
+    }
+}
