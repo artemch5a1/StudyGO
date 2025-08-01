d00d1b1ab1b28c2a61430beaf74eeb0a0154d3e1
praktika <praktika@PRAKTIKA>
2025-08-01 14:56:53 +0300

    Create UserProfileService.cs

---
+++ Develop/backend/StudyGO.API/StudyGO.Application/Services/Account/UserProfileService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using StudyGO.Application.Abstractions;
+using StudyGO.Contracts.Dtos.UserProfiles;
+using StudyGO.Core.Abstractions.Repositories;
+using StudyGO.Core.Abstractions.Services.Account;
+
+namespace StudyGO.Application.Services.Account
+{
+    public class UserProfileService : IUserProfileService
+    {
+        private IUserProfileRepository _userRepository;
+
+        private IMapper _mapper;
+
+        private ILogger _logger;
+
+        private IPasswordHasher _passwordHasher;
+
+        public UserProfileService(
+            IUserProfileRepository userRepository,
+            IMapper mapper,
+            ILogger logger,
+            IPasswordHasher passwordHasher
+        )
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _logger = logger;
+            _passwordHasher = passwordHasher;
+        }
+
+        public Task<List<UserProfileDto>> GetAllUserProfiles(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserProfileDto?> TryGetUserProfileById(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> TryRegistr(UserProfileRegistrDto profile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> TryUpdateUserProfile(UserProfileUpdateDto newProfile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
