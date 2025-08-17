1e5c18454ba6e943ce89720972da89db8c6f6629
Artem <alefa2000@mail.ru>
2025-08-17 16:55:14 +0300

    Delete VerificationServiceOptions.cs

--- Develop/backend/StudyGO.API/StudyGO.infrastructure/Verification/VerificationServiceOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace StudyGO.infrastructure.Extensions;
-
-public class VerificationServiceOptions
-{
-    public string BaseUrl { get; set; } = String.Empty;
-
-    public string MessageSubject { get; set; } = String.Empty;
-}
