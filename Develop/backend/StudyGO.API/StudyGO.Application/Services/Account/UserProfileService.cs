b3620698c0aea97e4db9a555cb46ec54b88a38c1
Artem <alefa2000@mail.ru>
2025-08-18 03:22:30 +0300

    service verified read

--- Develop/backend/StudyGO.API/StudyGO.Application/Services/Account/UserProfileService.cs
+++ Develop/backend/StudyGO.API/StudyGO.Application/Services/Account/UserProfileService.cs
@@ -64,7 +64,23 @@ namespace StudyGO.Application.Services.Account
             
             return result.MapDataTo(_mapper.Map<List<UserProfileDto>>);
         }
-
+        
+        public async Task<Result<List<UserProfileDto>>> GetAllUserVerifiedProfiles(
+            CancellationToken cancellationToken = default,
+            Pagination? value = null
+        )
+        {
+            _logger.LogInformation("Получение всех подтвержденных профилей пользователей");
+            
+            var result = value == null ? await _userRepository.GetAllVerified(cancellationToken) 
+                : await _userRepository.GetPagesVerified(value.Skip, value.Take,
+                cancellationToken);
+            
+            _logger.LogDebug("Получено {Count} подтвержденных профилей пользователей", result.Value?.Count ?? 0);
+            
+            return result.MapDataTo(_mapper.Map<List<UserProfileDto>>);
+        }
+        
         public async Task<Result<UserProfileDto?>> TryGetUserProfileById(
             Guid userId,
             CancellationToken cancellationToken = default
@@ -81,7 +97,24 @@ namespace StudyGO.Application.Services.Account
             
             return result.MapDataTo(_mapper.Map<UserProfileDto?>);
         }
-
+        
+        public async Task<Result<UserProfileDto?>> TryGetVerifiedUserProfileById(
+            Guid userId,
+            CancellationToken cancellationToken = default
+        )
+        {
+            _logger.LogInformation("Поиск профиля пользователя по ID: {UserId}", userId);
+            
+            var result = await _userRepository.GetByIdVerified(userId, cancellationToken);
+            
+            _logger.LogResult(result, 
+                "Подтвержденный профиль пользователя найден", 
+                "Профиль пользователя не найден среди подтвержденых", 
+                new { UserId = userId });
+            
+            return result.MapDataTo(_mapper.Map<UserProfileDto?>);
+        }
+        
         public async Task<Result<Guid>> TryRegistry(
             UserProfileRegistrDto profile,
             string confirmEmailEndpoint,
--- Develop/backend/StudyGO.API/StudyGO.Core/Abstractions/Services/Account/IUserProfileService.cs
+++ Develop/backend/StudyGO.API/StudyGO.Core/Abstractions/Services/Account/IUserProfileService.cs
@@ -14,5 +14,15 @@ namespace StudyGO.Core.Abstractions.Services.Account
 
         public Task<Result<List<UserProfileDto>>> GetAllUserProfiles(CancellationToken cancellationToken = default, 
             Pagination? value = null);
+
+        Task<Result<List<UserProfileDto>>> GetAllUserVerifiedProfiles(
+            CancellationToken cancellationToken = default,
+            Pagination? value = null
+        );
+
+        Task<Result<UserProfileDto?>> TryGetVerifiedUserProfileById(
+            Guid userId,
+            CancellationToken cancellationToken = default
+        );
     }
 }
