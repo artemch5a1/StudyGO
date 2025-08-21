f549e0936e5bf87f5dad00a8eba88183c0380177
praktika <praktika@PRAKTIKA>
2025-08-21 11:38:51 +0300

    registry services seed

---
+++ Develop/backend/StudyGO.API/StudyGO.API/Services/SeedData/ServiceBuilder.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using StudyGO.Core.Abstractions.DatabaseSeed;
+using StudyGO.infrastructure.Data;
+using StudyGO.infrastructure.Data.SeedServices;
+using StudyGO.infrastructure.Data.SeedServices.SeedOptions;
+
+namespace StudyGO.API.Services;
+
+public partial class ServiceBuilder
+{
+    private void ConfigureSeederAndServices()
+    {
+        _services.AddScoped<ISeedProvider, Seeder>();
+
+        _services.Configure<SubjectSeedOptions>(_configuration.GetSection("SeedData").GetSection("Subjects"));
+        
+        AddSeedService<SubjectsSeeder>();
+    }
+    
+    private void AddSeedService<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TSeedService>()
+        where TSeedService : class, ISeedService
+    {
+        _services.TryAddEnumerable(ServiceDescriptor.Scoped<ISeedService, TSeedService>());
+    }
+}
