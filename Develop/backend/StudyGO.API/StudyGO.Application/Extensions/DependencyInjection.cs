185d192dd48f77f958d6ae0f906d1df34421c0f4
Artem <alefa2000@mail.ru>
2025-08-31 00:39:53 +0300

    ResultEnforcementBehavior

--- Develop/backend/StudyGO.API/StudyGO.Application/Extensions/DependencyInjection.cs
+++ Develop/backend/StudyGO.API/StudyGO.Application/Extensions/DependencyInjection.cs
@@ -10,7 +10,9 @@ public static class DependencyInjection
     {
         services.AddMediatR(cfg => 
             cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
-
+        
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ResultEnforcementBehavior<,>));
+        
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
 
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
