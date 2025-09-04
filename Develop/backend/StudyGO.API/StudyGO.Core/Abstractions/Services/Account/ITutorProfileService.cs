fe5068c79596fc9fe1fffaf17f01c9ecf07d84bf
Artem <alefa2000@mail.ru>
2025-09-04 18:16:35 +0300

    Update ITutorProfileService.cs

--- Develop/backend/StudyGO.API/StudyGO.Core/Abstractions/Services/Account/ITutorProfileService.cs
+++ Develop/backend/StudyGO.API/StudyGO.Core/Abstractions/Services/Account/ITutorProfileService.cs
@@ -6,8 +6,6 @@ namespace StudyGO.Core.Abstractions.Services.Account
 {
     public interface ITutorProfileService
     {
-        public Task<Result<UserRegistryResponse>> TryRegistry(TutorProfileRegistrDto profile, string confirmEmailEndpoint, CancellationToken cancellationToken = default);
-
         public Task<Result<Guid>> TryUpdateUserProfile(TutorProfileUpdateDto newProfile, CancellationToken cancellationToken = default);
     }
 }
